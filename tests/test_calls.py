93e53cee5d38aab2153fe048272138d3ec97eba3
Nancy Hansen <nhansen@mail.nih.gov>
2024-06-10 16:42:29 -0400

    Finally starting to write meaningful unit tests

--- tests/test_calls.py
+++ tests/test_calls.py
@@ -1,7 +1,10 @@
 import pytest
 import subprocess
 import sys
+import os
+import pysam
 from q100bench import bench
+from q100bench import output
 from q100bench import seqparse
 from q100bench import bedtoolslib
 
@@ -10,7 +13,21 @@ def test_configs():
     configvals = bench.read_config_data(args)
     assert configvals['resourcedir'] == 'resources'
 
-def test_genstats():
-    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/testbam.bam', '-r', 'tests/testref.fa', '-q', 'tests/testquery.fa', '-p', 'tests/testrun'])
+def test_checkforprogs():
+    bench.check_for_bedtools()
+    no_rscript = bench.check_for_R()
+
+def test_createoutputdir():
+    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/testsort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
+    configvals = bench.read_config_data(args)
+    outputdir = output.create_output_directory(args.prefix)
+    assert os.path.isdir(outputdir)
+
+def test_readfasta():
+    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/testsort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
     configvals = bench.read_config_data(args)
 
+    refobj = pysam.FastaFile(args.reffasta)
+    queryobj = pysam.FastaFile(args.queryfasta)
+    outputfiles = output.name_output_files(args, args.prefix)
+    seqparse.write_genome_bedfiles(queryobj, refobj, args, configvals, outputfiles, {})
