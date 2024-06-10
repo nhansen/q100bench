536fb0a4363767eb19f3f7ebdf3bf3e2d393b371
Nancy Hansen <nhansen@mail.nih.gov>
2024-06-10 17:00:00 -0400

    Adding more tests

--- tests/test_calls.py
+++ tests/test_calls.py
@@ -6,6 +6,8 @@ import pysam
 from q100bench import bench
 from q100bench import output
 from q100bench import seqparse
+from q100bench import alignparse
+from q100bench import mummermethods
 from q100bench import bedtoolslib
 
 def test_configs():
@@ -18,16 +20,30 @@ def test_checkforprogs():
     no_rscript = bench.check_for_R()
 
 def test_createoutputdir():
-    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/testsort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
+    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/test.sort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
     configvals = bench.read_config_data(args)
     outputdir = output.create_output_directory(args.prefix)
     assert os.path.isdir(outputdir)
 
-def test_readfasta():
-    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/testsort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
+def test_writebedfiles():
+    args = bench.parse_arguments(['-c', 'tests/testconfig.txt', '-b', 'tests/test.sort.bam', '-r', 'tests/testbenchmark.fasta.gz', '-q', 'tests/testassembly.fasta.gz', '-p', 'tests/testrun'])
     configvals = bench.read_config_data(args)
 
     refobj = pysam.FastaFile(args.reffasta)
     queryobj = pysam.FastaFile(args.queryfasta)
     outputfiles = output.name_output_files(args, args.prefix)
     seqparse.write_genome_bedfiles(queryobj, refobj, args, configvals, outputfiles, {})
+
+    with open('tests/testrun/genome.test.bed', 'r') as gfh:
+        fields = gfh.readline().rstrip().split()
+        assert(int(fields[2]) == 9720)
+    with open('tests/testrun/atgcseq.test.bed', 'r') as gfh:
+        fields = gfh.readline().rstrip().split()
+        assert(int(fields[2]) == 9721)
+
+    alignobj = pysam.AlignmentFile(args.bam, "rb")
+    aligndata = alignparse.read_bam_aligns(alignobj, args.minalignlength)
+    rlis_aligndata = mummermethods.filter_aligns(aligndata, "target")
+
+    assert(len(aligndata) == 1)
+
